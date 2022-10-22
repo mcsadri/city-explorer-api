5c33e25e194798160c6c41444e19731da56bb16a
Manuch Sadri <colonquebang@hotmail.com>
2022-10-22 01:16:10 -0700

    completed lab 10 basic requirements. Attempted stretch goal of Task 1 to display timestamp, but was unable to get consistent function.

--- modules/movies.js
+++ modules/movies.js
@@ -1,26 +1,41 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache.js');
 
 function fetchMovies(request, response) {
-    console.log('searchQuery: ', request.query.searchQuery);
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${request.query.searchQuery}&page=1`;
-    axios
-        .get(movieUrl)
-        .then(movieResponse => {
-            // console.log(movieResponse.data);
-            const movies = movieResponse.data.results.map(movie => new Movie(movie));
-            response.status(200).send(movies);
-        })
-        .catch(error => {
-            console.error('fetchMovies axios error: ', error);
-            response.status(500).send('fetchMovies server error: ', error);
-            // next(error.message);
-        });
+    const searchQuery = request.query;
+    const key = 'movie-'+ searchQuery;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${request.query.searchQuery}&page=1`;
+    console.log('featchMovies url: ', url);
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 86400000) && cache[key].searchQuery == searchQuery) { // eslint-disable-line
+        console.log('fetchMovie() cache hit ');
+        response.status(200).send(cache[key]);
+    } else {
+        console.log('fetchMovies() cache miss');
+        cache[key] = {};
+        axios
+            .get(url)
+            .then(movieResponse => {
+                cache[key].timestamp = Date.now();
+                cache[key].date = Date().toLocaleString();
+                const movies = movieResponse.data.results.map(movie => new Movie(movie, cache[key].date));
+                cache[key] = movies;
+                cache[key].searchQuery = searchQuery;
+                response.status(200).send(cache[key]);
+            })
+            .catch (error => {
+                console.error('fetchMovies axios error: ', error);
+                response.status(500).send('fetchMovies server error: ', error);
+            });
+    }
+
+    return cache[key].data;
 }
 
 class Movie {
-    constructor(movie){
+    constructor(movie, date){
         this.id = movie.id;
         this.title = movie.title;
         this.overview = movie.overview.substring(0, 100) + '...';
@@ -29,6 +44,7 @@ class Movie {
         this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
         this.popularity = movie.popularity;
         this.released_on = movie.release_date;
+        this.cacheDate = date;
     }
 }
 
--- modules/weather.js
+++ modules/weather.js
@@ -6,64 +6,43 @@ const cache = require('./cache.js');
 function fetchWeather(request, response) {
     const {lat, lon} = request.query;
     const key = 'weather-' + lat + lon;
-    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=1`;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=7`;
     console.log('fetchWeather url: ', url);
 
-    console.log('Date.now: ', Date.now());
     if (cache[key] && (Date.now() - cache[key].timestamp < 3600000)) { // 1 hour
         console.log('fetchWeather() cache hit ');
-        // console.log('cache[key].timestamp: ', cache[key].timestamp);
-        console.log('weather cache age: ', (Date.now() - cache[key].timestamp));
         response.status(200).send(cache[key]);
     } else {
         console.log('fetchWeather() cache miss');
         cache[key] = {};
-        cache[key].timestamp = Date.now();
         axios
             .get(url)
             .then(weatherResponse => {
-                // console.log('weatherResponse.data: ', weatherResponse.data);
-                const forecasts = weatherResponse.data.data.map(day => new Weather(day));
-                cache[key] = forecasts;
                 cache[key].timestamp = Date.now();
-                // console.log('weather cache: ', cache[key]);
+                cache[key].date = Date().toLocaleString();
+                const cityName = weatherResponse.data.city_name + ', ' + weatherResponse.data.country_code;
+                const forecasts = weatherResponse.data.data.map(day => new Weather(day, cityName, cache[key].date));
+                cache[key] = forecasts;
                 response.status(200).send(cache[key]);
             })
-        // cache[key].data = axios
-        //     .get(url)
-        //     .then(weatherResponse => {
-        //         console.log('weatherResponse.data: ', weatherResponse.data);
-        //         parseWeather(weatherResponse.data);
-        //         cache[key].timestamp = Date.now();
-        //         console.log('weather cache: ', cache[key]);
-        //         response.status(200).send(cache[key]);
-        //     })
             .catch (error => {
-                console.error('fetchWeather axios error: ', error);
+                console.error('fetchWeather() axios error: ', error);
                 response.status(500).send('fetchWeather server error: ', error);
             });
     }
+
     return cache[key].data;
 }
 
-// function parseWeather(weatherData) {
-//     try {
-//         const weatherSummaries = weatherData.data.map(day => {
-//             return new Weather(day);
-//         });
-//         return Promise.resolve(weatherSummaries);
-//     } catch (e) {
-//         return Promise.reject(e);
-//     }
-// }
-
 class Weather {
-    constructor(day){
+    constructor(day, cityName, date){
         this.id = day.sunrise_ts;
-        this.date = day.datetime;
+        this.cityName = cityName;
+        this.dateTime = day.datetime;
         this.description = day.weather.description;
         this.lowTemp = day.low_temp + ' °C';
         this.highTemp = day.high_temp + ' °C';
+        this.cacheDate = date;
     }
 }
 
